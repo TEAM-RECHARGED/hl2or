3e1dd859e519834bd1ffce334ae1f50981662650
GuestSneezeOSDev <163439609+GuestSneezeOSDev@users.noreply.github.com>
2024-12-24 14:08:10 +0400

    BulletTime

--- src/game/client/hl2or/cmds.cpp
+++ src/game/client/hl2or/cmds.cpp
@@ -1,15 +1,11 @@
 //========= Copyright Half-Life 2 Overcharged: Redux, All rights reserved. ============//
 //
-// Purpose: 
-//
+// Purpose: Implements bullet time functionality for the client.
+// This functionality is in beta and might cause irreversible issues if not handled properly.
+// Use host_timescale 1 to restore normal time after enabling bullet time.
 //=============================================================================//
 #include "cbase.h"
 #include "convar.h"
+
 #ifdef CLIENT_DLL
-ConVar bullettime("bulltime", 0, 1, "Enable BulletTime");
-void bulletemit(void)
-{
-	engine->ClientCmd("host_timescale 0.6");
-	Msg("Note that this is in beta and can cause irreversable issues. to restore normal time run host_timescale 1 or run this command again, thanks in advance");
-}
 #endif // CLIENT_DLL
