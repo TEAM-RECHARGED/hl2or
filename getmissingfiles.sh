1f6fa8e3b53b028987cf62aec9491a151f64c9ab
Mohamed <mohamed.o.ashraf173@gmail.com>
2025-01-29 14:59:12 +0400

    improvements

--- getmissingfiles.sh
+++ getmissingfiles.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 OS=`uname`
+HL2OR_PATH=`game/hl2or/`
 if [`$1` == `--models`]; then
     wget https://ia801408.us.archive.org/3/items/hl2oc/OVERCHARGED_2.0.zip
     unzip OVERCHARGED_2.0.zip
     mv OVERCHARGED_2.0 overcharged
     cd overcharged
-    mv materials/* game/hl2or/materials/
-    mv models/* game/hl2or/models/
+    mv materials/* $HL2OR_PATH/materials/
+    mv models/* $HL2OR_PATH/models/
+    rm -rf overcharged
 elif [`$1` == `--maps`]; then
     if [ $OS == "Linux" ]; then
-        cp -r ~/.steam/steamapps/common/Half-Life 2/maps/* game/hl2or/maps/*
+        cp -r ~/.steam/steamapps/common/Half-Life 2/maps/* $HL2OR_PATH/maps/*
     else
         echo "Unknown OS, using Windows."
-        cp -r C:/Program Files (x86)/Steam/steamapps/common/Half-Life 2/maps/* game/hl2or/maps/*
+        cp -r C:/Program Files (x86)/Steam/steamapps/common/Half-Life 2/maps/* $HL2OR_PATHs/maps/*
     fi
 fi
